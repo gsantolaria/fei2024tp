2fd15f222725135d645246cd0603bb1c67c63a92
guillermo santolaria <guillote.s07@gmail.com>
2024-05-25 09:30:33 -0300

    tp2, punto 4) Creacion de Tablas

--- backend/aulas/models.py
+++ backend/aulas/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=128)
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=128)
+    apellido = models.CharField(max_length=128)
+    mostrar = models.CharField(max_length=256)
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=128)
+    cant_alumnos = models.IntegerField(default=5)
+    id_carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    id_profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+
+class Aula(models.Model):
+    descripcion = models.CharField(max_length=128)
+    ubicacion = models.CharField(max_length=128)
+    cant_proyector = models.IntegerField(default=0)
+    aforo = models.IntegerField(default=0)
+    es_climatizada = models.BooleanField(default=False)
